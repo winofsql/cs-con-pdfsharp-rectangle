e68ba6134b1ef2065e7c0cd0434a66b1006d1e73
sworc <lightbox@nifty.com>
2021-10-27 22:21:17 +0900

    Update FontResolver.cs

--- FontResolver.cs
+++ FontResolver.cs
@@ -1,55 +1,56 @@
-﻿using PdfSharp.Fonts;
+using PdfSharp.Fonts;
 using System.Diagnostics;
 using System.IO;
 
-namespace cs_con_pdfsharp {
+namespace cs_con_pdfsharp_rectangle {
+
     class FontResolver : IFontResolver {
-		byte[] IFontResolver.GetFont(string faceName)
-		{
-			switch (faceName)
-			{
-				case "けいふぉんと":
-					return LoadFontData("keifont.ttf");
+        byte[] IFontResolver.GetFont(string faceName)
+        {
+            switch (faceName)
+            {
+                case "けいふぉんと":
+                    return LoadFontData("keifont.ttf");
 
-				case "ラノベPOP":
-					return LoadFontData("LightNovelPOP.ttf");
-			}
+                case "ラノベPOP":
+                    return LoadFontData("LightNovelPOP.ttf");
+            }
 
-			return null;
-		}
-		private byte[] LoadFontData(string resourceName)
-		{
-			try
-			{
-				using (FileStream stream = new FileStream(resourceName, FileMode.Open, FileAccess.Read))
-				{
-					byte[] data = new byte[stream.Length];
-					stream.Read(data, 0, (int)stream.Length);
-					return data;
-				}
-			}
-			catch (FileNotFoundException ex)
-			{
-				Debug.WriteLine(ex.Message);
-			}
+            return null;
+        }
+        private byte[] LoadFontData(string resourceName)
+        {
+            try
+            {
+                using (FileStream stream = new FileStream(resourceName, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] data = new byte[stream.Length];
+                    stream.Read(data, 0, (int)stream.Length);
+                    return data;
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		FontResolverInfo IFontResolver.ResolveTypeface(string familyName, bool isBold, bool isItalic)
-		{
-			switch (familyName)
-			{
-				case "けいふぉんと":
-					return new FontResolverInfo("けいふぉんと");
+        FontResolverInfo IFontResolver.ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            switch (familyName)
+            {
+                case "けいふぉんと":
+                    return new FontResolverInfo("けいふぉんと");
 
-				case "ラノベPOP":
-					return new FontResolverInfo("ラノベPOP");
+                case "ラノベPOP":
+                    return new FontResolverInfo("ラノベPOP");
 
-					
-			}
+                    
+            }
 
-			return null;
-		}
-	}
+            return null;
+        }
+    }
 }
